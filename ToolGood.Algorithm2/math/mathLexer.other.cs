2c547ca5d86153d7fa14485b61fb0932b21fa08e
toolgood <toolgood@qq.com>
2020-07-02 18:46:07 +0800

    修改 提升速度

---
+++ ToolGood.Algorithm2/math/mathLexer.other.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+partial class mathLexer
+{
+    public const int MUL = 8;
+    public const int DIV = 9;
+    public const int ADD = 10;
+    public const int SUB = 11;
+    public const int MOD_2 = 12;
+    public const int MERGE = 13;
+    public const int LT = 15;
+    public const int LE = 16;
+    public const int GT = 17;
+    public const int GE = 18;
+    public const int ET = 19;
+    public const int NT = 20;
+
+
+
+}
+
