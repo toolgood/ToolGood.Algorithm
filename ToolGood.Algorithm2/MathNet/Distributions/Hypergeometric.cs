fc99202136e2b78116052a043fd426208de7fd17
lzj <lzj@check.com>
2020-07-03 13:38:30 +0800

    修改

--- ToolGood.Algorithm2/MathNet/Distributions/Hypergeometric.cs
+++ ToolGood.Algorithm2/MathNet/Distributions/Hypergeometric.cs
@@ -24,29 +24,13 @@ namespace ToolGood.Algorithm.MathNet.Numerics.Distributions
         /// <returns>the probability mass at location <paramref name="k"/>.</returns>
         public static double PMF(int population, int success, int draws, int k)
         {
-            if (!(population >= 0 && success >= 0 && draws >= 0 && success <= population && draws <= population)) {
-                throw new ArgumentException("InvalidDistributionParameters");
-            }
+            //if (!(population >= 0 && success >= 0 && draws >= 0 && success <= population && draws <= population)) {
+            //    throw new ArgumentException("InvalidDistributionParameters");
+            //}
 
             return SpecialFunctions.Binomial(success, k) * SpecialFunctions.Binomial(population - success, draws - k) / SpecialFunctions.Binomial(population, draws);
         }
 
-        /// <summary>
-        /// Computes the log probability mass (lnPMF) at k, i.e. ln(P(X = k)).
-        /// </summary>
-        /// <param name="k">The location in the domain where we want to evaluate the log probability mass function.</param>
-        /// <param name="population">The size of the population (N).</param>
-        /// <param name="success">The number successes within the population (K, M).</param>
-        /// <param name="draws">The number of draws without replacement (n).</param>
-        /// <returns>the log probability mass at location <paramref name="k"/>.</returns>
-        public static double PMFLn(int population, int success, int draws, int k)
-        {
-            if (!(population >= 0 && success >= 0 && draws >= 0 && success <= population && draws <= population)) {
-                throw new ArgumentException("InvalidDistributionParameters");
-            }
-
-            return SpecialFunctions.BinomialLn(success, k) + SpecialFunctions.BinomialLn(population - success, draws - k) - SpecialFunctions.BinomialLn(population, draws);
-        }
-
+ 
     }
 }
