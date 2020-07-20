a3ebe571b3ec702dfe392c9f7693d9d3cd7176ce
lzj <lzj@check.com>
2020-07-20 10:48:31 +0800

    添加 BenchmarkDotNet 性能测试

---
+++ csharp/ToolGood.Algorithm2.PerformanceTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using ToolGood.Algorithm;
+using ToolGood.Algorithm2.PerformanceTest.Test;
+
+namespace ToolGood.Algorithm2.PerformanceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                AlgorithmEngine engine = new AlgorithmEngine();
+                var c = engine.TryEvaluate("2+3", 0);
+            }
+
+            var boKar = BenchmarkRunner.Run<AlgorithmEngineTest>();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ csharp/ToolGood.Algorithm2.PerformanceTest/Test/AlgorithmEngineTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using ToolGood.Algorithm;
+
+namespace ToolGood.Algorithm2.PerformanceTest.Test
+{
+    public class AlgorithmEngineTest
+    {
+        [Benchmark]
+        public void Test_Add()
+        {
+            AlgorithmEngine engine = new AlgorithmEngine();
+            var c = engine.TryEvaluate("2+3", 0);
+        }
+        [Benchmark]
+        public void Test_Sub()
+        {
+            AlgorithmEngine engine = new AlgorithmEngine();
+            var c = engine.TryEvaluate("2-3", 0);
+        }
+
+        [Benchmark]
+        public void Test_Mul()
+        {
+            AlgorithmEngine engine = new AlgorithmEngine();
+            var c = engine.TryEvaluate("2*3", 0);
+        }
+        [Benchmark]
+        public void Test_Div()
+        {
+            AlgorithmEngine engine = new AlgorithmEngine();
+            var c = engine.TryEvaluate("2/3", 0);
+        }
+
+
+    }
+}
