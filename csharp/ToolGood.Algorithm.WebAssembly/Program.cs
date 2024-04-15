ca36c2f986e2fc3f643e46c4e158a4ed1d9e18fe
linzhijun <at6180686@qq.com>
2024-04-15 09:20:51 +0800

    添加 ToolGood.Algorithm.WebAssembly 项目

---
+++ csharp/ToolGood.Algorithm.WebAssembly/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ToolGood.Algorithm.WebAssembly
+{
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.JSInterop;
+    using ToolGood.Algorithm;
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+        }
+
+       
+        ///// <summary>
+        ///// 给js调用的函数
+        ///// </summary>
+        ///// <param name="i"></param>
+        ///// <param name="j"></param>
+        ///// <returns></returns>
+        //[JSInvokable]
+        //public static byte[] Add(int i, int j)
+        //{
+        //    return new byte[] { (byte)i, (byte)j };
+        //}
+
+        [JSInvokable]
+        public static string TryEvaluate(string exp)
+        {
+            var ae = new AlgorithmEngine();
+            return ae.TryEvaluate(exp, null);
+        }
+    }
+}
---
+++ csharp/ToolGood.Algorithm2.WebTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+
+namespace ToolGood.Algorithm2.WebTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment()) {
+                app.UseExceptionHandler("/Error");
+            }
+            var provider = new FileExtensionContentTypeProvider();
+            provider.Mappings[".dll"] = "application/octet-stream";//配置添加新的映射关系
+            provider.Mappings[".gz"] = "application/octet-stream";
+            provider.Mappings[".dat"] = "application/octet-stream";
+            provider.Mappings[".blat"] = "application/octet-stream";
+            provider.Mappings[".pdb"] = "application/octet-stream";
+            provider.Mappings[".wasm"] = "application/octet-stream";
+            app.UseStaticFiles(new StaticFileOptions {
+                ContentTypeProvider = provider,//应用新的映射关系
+            });
+			
+
+
+			app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
