18b5f6f92885e57df69a92978acb58ba6bf4c6be
linzhijun <at6180686@qq.com>
2025-12-09 10:37:33 +0800

    fix

--- csharp/BenchmarkSuite1/AlgorithmEngineBenchmarks.cs
+++ csharp/BenchmarkSuite1/AlgorithmEngineBenchmarks.cs
@@ -1,200 +1,202 @@
-using BenchmarkDotNet.Attributes;
-using ToolGood.Algorithm;
-using Microsoft.VSDiagnostics;
+//using BenchmarkDotNet.Attributes;
+//using Microsoft.VSDiagnostics;
+//using ToolGood.Algorithm;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
 
-[CPUUsageDiagnoser]
-public class AlgorithmEngineBenchmarks
-{
-    private AlgorithmEngine engine;
-    [GlobalSetup]
-    public void Setup()
-    {
-        engine = new AlgorithmEngine();
-    }
+//[CPUUsageDiagnoser]
+//public class AlgorithmEngineBenchmarks
+//{
+//    private AlgorithmEngine engine;
+//    [GlobalSetup]
+//    public void Setup()
+//    {
+//        engine = new AlgorithmEngine();
+//    }
 
-    [Benchmark]
-    public string UrlEncodeBenchmark()
-    {
-        return engine.TryEvaluate("UrlEncode('&=我中国人 >||')", "");
-    }
 
-    //[Benchmark]
-    //public string UrlDecodeBenchmark()
-    //{
-    //    return engine.TryEvaluate("UrlDecode('%26%3d%e6%88%91%e4%b8%ad%e5%9b%bd%e4%ba%ba+%3e%7c%7c')", "");
-    //}
+//    [Benchmark]
+//    public string UrlEncodeBenchmark()
+//    {
+//        return engine.TryEvaluate("UrlEncode('&=我中国人 >||')", "");
+//    }
 
-    //[Benchmark]
-    //public string HtmlEncodeBenchmark()
-    //{
-    //    return engine.TryEvaluate("HtmlEncode('&=我中国人 >||')", "");
-    //}
+//    [Benchmark]
+//    public string UrlDecodeBenchmark()
+//    {
+//        return engine.TryEvaluate("UrlDecode('%26%3d%e6%88%91%e4%b8%ad%e5%9b%bd%e4%ba%ba+%3e%7c%7c')", "");
+//    }
 
-    //[Benchmark]
-    //public string HtmlDecodeBenchmark()
-    //{
-    //    return engine.TryEvaluate("HtmlDecode('&amp;=我中国人 &gt;||')", "");
-    //}
+//    [Benchmark]
+//    public string HtmlEncodeBenchmark()
+//    {
+//        return engine.TryEvaluate("HtmlEncode('&=我中国人 >||')", "");
+//    }
 
-    //[Benchmark]
-    //public string Md5Benchmark()
-    //{
-    //    return engine.TryEvaluate("Md5('&=我中国人 >||')", "");
-    //}
+//    [Benchmark]
+//    public string HtmlDecodeBenchmark()
+//    {
+//        return engine.TryEvaluate("HtmlDecode('&amp;=我中国人 &gt;||')", "");
+//    }
 
-    //[Benchmark]
-    //public string Sha1Benchmark()
-    //{
-    //    return engine.TryEvaluate("Sha1('&=我中国人 >||')", "");
-    //}
+//    [Benchmark]
+//    public string Md5Benchmark()
+//    {
+//        return engine.TryEvaluate("Md5('&=我中国人 >||')", "");
+//    }
 
-    //[Benchmark]
-    //public string Sha256Benchmark()
-    //{
-    //    return engine.TryEvaluate("Sha256('&=我中国人 >||')", "");
-    //}
+//    [Benchmark]
+//    public string Sha1Benchmark()
+//    {
+//        return engine.TryEvaluate("Sha1('&=我中国人 >||')", "");
+//    }
 
-    //[Benchmark]
-    //public string Sha512Benchmark()
-    //{
-    //    return engine.TryEvaluate("Sha512('&=我中国人 >||')", "");
-    //}
+//    [Benchmark]
+//    public string Sha256Benchmark()
+//    {
+//        return engine.TryEvaluate("Sha256('&=我中国人 >||')", "");
+//    }
 
-    //[Benchmark]
-    //public string Crc32Benchmark()
-    //{
-    //    return engine.TryEvaluate("Crc32('&=我中国人 >||')", "");
-    //}
+//    [Benchmark]
+//    public string Sha512Benchmark()
+//    {
+//        return engine.TryEvaluate("Sha512('&=我中国人 >||')", "");
+//    }
 
-    //[Benchmark]
-    //public string TextToBase64Benchmark()
-    //{
-    //    return engine.TryEvaluate("TextToBase64('&=我中国人 >||')", "");
-    //}
+//    [Benchmark]
+//    public string Crc32Benchmark()
+//    {
+//        return engine.TryEvaluate("Crc32('&=我中国人 >||')", "");
+//    }
 
-    //[Benchmark]
-    //public string Base64ToTextBenchmark()
-    //{
-    //    return engine.TryEvaluate("Base64ToText('Jj3miJHkuK3lm73kurogPnx8')", "");
-    //}
+//    [Benchmark]
+//    public string TextToBase64Benchmark()
+//    {
+//        return engine.TryEvaluate("TextToBase64('&=我中国人 >||')", "");
+//    }
 
-    //[Benchmark]
-    //public string RegexBenchmark()
-    //{
-    //    return engine.TryEvaluate("Regex('abcd','a.*c')", "");
-    //}
+//    [Benchmark]
+//    public string Base64ToTextBenchmark()
+//    {
+//        return engine.TryEvaluate("Base64ToText('Jj3miJHkuK3lm73kurogPnx8')", "");
+//    }
 
-    //[Benchmark]
-    //public bool IsRegexBenchmark()
-    //{
-    //    return engine.TryEvaluate("IsRegex('abcd','a.*c')", false);
-    //}
+//    [Benchmark]
+//    public string RegexBenchmark()
+//    {
+//        return engine.TryEvaluate("Regex('abcd','a.*c')", "");
+//    }
 
-    //[Benchmark]
-    //public string GuidBenchmark()
-    //{
-    //    return engine.TryEvaluate("Guid()", "");
-    //}
+//    [Benchmark]
+//    public bool IsRegexBenchmark()
+//    {
+//        return engine.TryEvaluate("IsRegex('abcd','a.*c')", false);
+//    }
 
-    //[Benchmark]
-    //public string HmacMd5Benchmark()
-    //{
-    //    return engine.TryEvaluate("HmacMd5('&=我中国人 >||','12')", "");
-    //}
+//    [Benchmark]
+//    public string GuidBenchmark()
+//    {
+//        return engine.TryEvaluate("Guid()", "");
+//    }
 
-    //[Benchmark]
-    //public string HmacSha1Benchmark()
-    //{
-    //    return engine.TryEvaluate("HmacSha1('&=我中国人 >||','12')", "");
-    //}
+//    [Benchmark]
+//    public string HmacMd5Benchmark()
+//    {
+//        return engine.TryEvaluate("HmacMd5('&=我中国人 >||','12')", "");
+//    }
 
-    //[Benchmark]
-    //public string HmacSha256Benchmark()
-    //{
-    //    return engine.TryEvaluate("HmacSha256('&=我中国人 >||','12')", "");
-    //}
+//    [Benchmark]
+//    public string HmacSha1Benchmark()
+//    {
+//        return engine.TryEvaluate("HmacSha1('&=我中国人 >||','12')", "");
+//    }
 
-    //[Benchmark]
-    //public string HmacSha512Benchmark()
-    //{
-    //    return engine.TryEvaluate("HmacSha512('&=我中国人 >||','12')", "");
-    //}
+//    [Benchmark]
+//    public string HmacSha256Benchmark()
+//    {
+//        return engine.TryEvaluate("HmacSha256('&=我中国人 >||','12')", "");
+//    }
 
-    //[Benchmark]
-    //public string TrimStartBenchmark()
-    //{
-    //    return engine.TryEvaluate("TrimStart(' 123 ')", "");
-    //}
+//    [Benchmark]
+//    public string HmacSha512Benchmark()
+//    {
+//        return engine.TryEvaluate("HmacSha512('&=我中国人 >||','12')", "");
+//    }
 
-    //[Benchmark]
-    //public string TrimEndBenchmark()
-    //{
-    //    return engine.TryEvaluate("TrimEnd(' 123 ')", "");
-    //}
+//    [Benchmark]
+//    public string TrimStartBenchmark()
+//    {
+//        return engine.TryEvaluate("TrimStart(' 123 ')", "");
+//    }
 
-    //[Benchmark]
-    //public int IndexOfBenchmark()
-    //{
-    //    return engine.TryEvaluate("IndexOf('abcd','cd')", -1);
-    //}
+//    [Benchmark]
+//    public string TrimEndBenchmark()
+//    {
+//        return engine.TryEvaluate("TrimEnd(' 123 ')", "");
+//    }
 
-    //[Benchmark]
-    //public int LastIndexOfBenchmark()
-    //{
-    //    return engine.TryEvaluate("LastIndexOf('abcd','cd')", -1);
-    //}
+//    [Benchmark]
+//    public int IndexOfBenchmark()
+//    {
+//        return engine.TryEvaluate("IndexOf('abcd','cd')", -1);
+//    }
 
-    //[Benchmark]
-    //public string SplitBenchmark()
-    //{
-    //    return engine.TryEvaluate("Split('1,2,3,4',',')[3]", "");
-    //}
+//    [Benchmark]
+//    public int LastIndexOfBenchmark()
+//    {
+//        return engine.TryEvaluate("LastIndexOf('abcd','cd')", -1);
+//    }
 
-    //[Benchmark]
-    //public string JoinBenchmark()
-    //{
-    //    return engine.TryEvaluate("Join(',',1,2,5,6)", "");
-    //}
+//    [Benchmark]
+//    public string SplitBenchmark()
+//    {
+//        return engine.TryEvaluate("Split('1,2,3,4',',')[3]", "");
+//    }
 
-    //[Benchmark]
-    //public string SubstringBenchmark()
-    //{
-    //    return engine.TryEvaluate("Substring('123456789',1,2)", "");
-    //}
+//    [Benchmark]
+//    public string JoinBenchmark()
+//    {
+//        return engine.TryEvaluate("Join(',',1,2,5,6)", "");
+//    }
 
-    //[Benchmark]
-    //public bool StartsWithBenchmark()
-    //{
-    //    return engine.TryEvaluate("StartsWith('123456789','12')", false);
-    //}
+//    [Benchmark]
+//    public string SubstringBenchmark()
+//    {
+//        return engine.TryEvaluate("Substring('123456789',1,2)", "");
+//    }
 
-    //[Benchmark]
-    //public bool EndsWithBenchmark()
-    //{
-    //    return engine.TryEvaluate("EndsWith('123456789','89')", false);
-    //}
+//    [Benchmark]
+//    public bool StartsWithBenchmark()
+//    {
+//        return engine.TryEvaluate("StartsWith('123456789','12')", false);
+//    }
 
-    //[Benchmark]
-    //public string RemoveStartBenchmark()
-    //{
-    //    return engine.TryEvaluate("RemoveStart('123456789','12')", "");
-    //}
+//    [Benchmark]
+//    public bool EndsWithBenchmark()
+//    {
+//        return engine.TryEvaluate("EndsWith('123456789','89')", false);
+//    }
 
-    //[Benchmark]
-    //public string RemoveEndBenchmark()
-    //{
-    //    return engine.TryEvaluate("RemoveEnd('123456789','89')", "");
-    //}
+//    [Benchmark]
+//    public string RemoveStartBenchmark()
+//    {
+//        return engine.TryEvaluate("RemoveStart('123456789','12')", "");
+//    }
 
-    //[Benchmark]
-    //public string JsonBenchmark()
-    //{
-    //    return engine.TryEvaluate("json('{\"Name\":\"William Shakespeare\",\"Age\":51}').Age", "");
-    //}
+//    [Benchmark]
+//    public string RemoveEndBenchmark()
+//    {
+//        return engine.TryEvaluate("RemoveEnd('123456789','89')", "");
+//    }
 
-    //[Benchmark]
-    //public bool HasBenchmark()
-    //{
-    //    return engine.TryEvaluate("Has(json('{\"Name\":\"William\"}'), 'Name')", false);
-    //}
-}
+//    [Benchmark]
+//    public string JsonBenchmark()
+//    {
+//        return engine.TryEvaluate("json('{\"Name\":\"William Shakespeare\",\"Age\":51}').Age", "");
+//    }
+
+//    [Benchmark]
+//    public bool HasBenchmark()
+//    {
+//        return engine.TryEvaluate("Has(json('{\"Name\":\"William\"}'), 'Name')", false);
+//    }
+//}
---
+++ csharp/BenchmarkSuite1/IfBeforeConversionBenchmarks.cs
@@ -0,0 +1,104 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.VSDiagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToolGood.Algorithm;
+
+namespace BenchmarkSuite1
+{
+    [CPUUsageDiagnoser]
+    public class IfBeforeConversionBenchmarks
+    {
+        [Benchmark]
+        public Operand String2Number_1()
+        {
+            var obj = Operand.Create("666");
+            if (obj.IsNotNumber) {
+                obj = obj.ToNumber("It can't be converted to number!");
+                if (obj.IsError) {
+                    return obj;
+                }
+            }
+            return obj;
+        }
+        [Benchmark]
+        public Operand String2Number_1_2()
+        {
+            var obj = Operand.Create("666");
+            obj = obj.ToNumber("It can't be converted to number!");
+            if (obj.IsError) {
+                return obj;
+            }
+            return obj;
+        }
+        [Benchmark]
+        public Operand String2String_1()
+        {
+            var obj = Operand.Create("666");
+            if (obj.IsNotNumber) {
+                obj = obj.ToText("It can't be converted to number!");
+                if (obj.IsError) {
+                    return obj;
+                }
+            }
+            return obj;
+        }
+        [Benchmark]
+        public Operand String2String_1_2()
+        {
+            var obj = Operand.Create("666");
+            obj = obj.ToText("It can't be converted to number!");
+            if (obj.IsError) {
+                return obj;
+            }
+            return obj;
+        }
+
+        [Benchmark]
+        public Operand Number2Number_1()
+        {
+            var obj = Operand.Create(666);
+            if (obj.IsNotNumber) {
+                obj = obj.ToNumber("It can't be converted to number!");
+                if (obj.IsError) {
+                    return obj;
+                }
+            }
+            return obj;
+        }
+        [Benchmark]
+        public Operand Number2Number_1_1()
+        {
+            var obj = Operand.Create(666);
+            obj = obj.ToNumber("It can't be converted to number!");
+            if (obj.IsError) {
+                return obj;
+            }
+            return obj;
+        }
+        [Benchmark]
+        public Operand Number2String_1()
+        {
+            var obj = Operand.Create(666);
+            if (obj.IsNotText) {
+                obj = obj.ToText("It can't be converted to number!");
+                if (obj.IsError) {
+                    return obj;
+                }
+            }
+            return obj;
+        }
+        [Benchmark]
+        public Operand Number2String_1_1()
+        {
+            var obj = Operand.Create(666);
+            obj = obj.ToText("It can't be converted to number!");
+            if (obj.IsError) {
+                return obj;
+            }
+            return obj;
+        }
+
+    }
+}
