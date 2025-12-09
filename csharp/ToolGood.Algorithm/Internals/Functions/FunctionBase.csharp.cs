6d3b288c4012dc9a79868e9cb0727c3bbf8f72d5
LinZhijun <toolgood@qq.com>
2025-12-09 18:58:57 +0800

    fix

--- csharp/ToolGood.Algorithm/Internals/Functions/FunctionBase.csharp.cs
+++ csharp/ToolGood.Algorithm/Internals/Functions/FunctionBase.csharp.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using ToolGood.Algorithm.LitJson;
 
 namespace ToolGood.Algorithm.Internals.Functions
 {
--- csharp/ToolGood.Algorithm/Internals/Functions/FunctionBase.value.cs
+++ csharp/ToolGood.Algorithm/Internals/Functions/FunctionBase.value.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using ToolGood.Algorithm.LitJson;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace ToolGood.Algorithm.Internals.Functions
 {
