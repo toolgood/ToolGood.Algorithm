ad3a3e31a6565db0a3de58917b807c9a81011bb6
LinZhijun <toolgood@qq.com>
2026-01-24 11:33:59 +0800

    fix

--- csharp/ToolGood.Algorithm.Test/ConditionTrees/ConditionTreeTest.cs
+++ csharp/ToolGood.Algorithm.Test/ConditionTrees/ConditionTreeTest.cs
@@ -139,5 +139,43 @@ namespace ToolGood.Algorithm2.Test.ConditionTrees
             Assert.AreEqual("dd=ss", txt.Substring(t3.Start, t3.End - t3.Start + 1));
             Assert.AreEqual("[tt]==22", txt.Substring(t4.Start, t4.End - t4.Start + 1));
         }
-    }
+
+
+		[Test]
+		public void Test10()
+		{
+			string txt = "AA && (dd=ss || [tt]==22)";
+			var tree = ConditionTree.Parse(txt);
+
+			Assert.AreEqual(tree.Type, ConditionTreeType.And);
+			var t1 = tree.Nodes[0];
+			var t2 = tree.Nodes[1];
+			Assert.AreEqual(t2.Type, ConditionTreeType.Or);
+			var t3 = t2.Nodes[0];
+			var t4 = t2.Nodes[1];
+
+			Assert.AreEqual("AA", txt.Substring(t1.Start, t1.End - t1.Start + 1));
+			Assert.AreEqual("dd=ss", txt.Substring(t3.Start, t3.End - t3.Start + 1));
+			Assert.AreEqual("[tt]==22", txt.Substring(t4.Start, t4.End - t4.Start + 1));
+		}
+
+
+		[Test]
+		public void Test11()
+		{
+			string txt = "1 && (dd=ss || [tt]==22)";
+			var tree = ConditionTree.Parse(txt);
+
+			Assert.AreEqual(tree.Type, ConditionTreeType.And);
+			var t1 = tree.Nodes[0];
+			var t2 = tree.Nodes[1];
+			Assert.AreEqual(t2.Type, ConditionTreeType.Or);
+			var t3 = t2.Nodes[0];
+			var t4 = t2.Nodes[1];
+
+			Assert.AreEqual("1", txt.Substring(t1.Start, t1.End - t1.Start + 1));
+			Assert.AreEqual("dd=ss", txt.Substring(t3.Start, t3.End - t3.Start + 1));
+			Assert.AreEqual("[tt]==22", txt.Substring(t4.Start, t4.End - t4.Start + 1));
+		}
+	}
 }
