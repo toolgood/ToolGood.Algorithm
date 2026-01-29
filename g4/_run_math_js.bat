47d3b6a7160716c4fd047debd80afa2ed16ac194
linzhijun <at6180686@qq.com>
2026-01-29 15:05:43 +0800

    fix

--- g4/_run_math_js.bat
+++ g4/_run_math_js.bat
@@ -1,3 +1,3 @@
-java -jar antlr-4.9.3-complete math.g4 -no-listener -no-visitor  -Dlanguage=JavaScript -long-messages -o antlr4
+java -jar antlr-4.13.2-complete.jar math.g4 -no-listener -visitor  -Dlanguage=JavaScript -long-messages -o antlr4
 
 pasue
