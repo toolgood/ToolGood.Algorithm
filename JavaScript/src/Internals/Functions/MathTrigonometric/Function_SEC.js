3eba04e401ffcc0329d4c6f219804a29bfb6e1c7
LinZhijun <toolgood@qq.com>
2026-01-30 21:58:34 +0800

    fix

--- JavaScript/src/Internals/Functions/MathTrigonometric/Function_SEC.js
+++ JavaScript/src/Internals/Functions/MathTrigonometric/Function_SEC.js
@@ -17,7 +17,7 @@ class Function_SEC extends Function_1 {
         }
         let d = Math.cos(args1.DoubleValue);
         if (d === 0) {
-            return Operand.Error('Function \'{0}\') div 0 error!', 'Sec');
+            return Operand.Error('Function \'{0}\' div 0 error!', 'Sec');
         }
         return Operand.Create(1.0 / d);
     }
