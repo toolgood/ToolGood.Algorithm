610cac559c50a65ab16323a43de74654c5db4d3d
linzhijun <at6180686@qq.com>
2026-01-30 16:31:11 +0800

    fix

---
+++ JavaScript/src/Internals/Functions/StringCache.js
@@ -0,0 +1,6 @@
+export const StringCache = {
+  Function_parameter_error1: "Function '{0}' parameter {1} is error!" ,
+  Function_parameter_error2: "Function {0} parameter {1} is error!" ,
+  Function_error1:'Function \'{0}\'is error!',
+  Function_error2:'Function {0} is error!'
+};
