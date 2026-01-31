d2b9601f3a08cd0798da3d9fdd6826d8a04f8f53
linzhijun <at6180686@qq.com>
2026-01-31 11:11:48 +0800

    fix

--- tools/Antlr4Helper.JavaScriptHelper/Program.cs
+++ tools/Antlr4Helper.JavaScriptHelper/Program.cs
@@ -1,55 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Antlr4Helper.JavaScriptHelper.Helpers;
 
 namespace Antlr4Helper.JavaScriptHelper
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var filePath = Path.GetFullPath(@"..\..\..\..\..\..\g4\bin\mathParser.js");
-            var text = File.ReadAllText(filePath);
-            text = ParserJsHelper.ReplaceNumber(text);
-            text = ParserJsHelper.ReplaceKey(text);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var filePath = Path.GetFullPath(@"..\..\..\..\..\g4\antlr4\mathParser.js");
+			var text = File.ReadAllText(filePath);
+			text = text.Replace("import antlr4 from 'antlr4';", "import antlr4 from '../antlr4/index.web.js';");
+			text = text.Replace("mathParser.EOF = antlr4.Token.EOF;", "");
+			text = Regex.Replace(text, @"mathParser\..*?_funContext = .*?_funContext;", "");
+			text = Regex.Replace(text, @"this\.state = \d+;[\r\n\t ]*this.match", "this.match");
+			var ms = Regex.Matches(text, @"(mathParser\..*) = (\d+);");
+			Dictionary<string, string> dict = new Dictionary<string, string>();
+			dict["mathParser.EOF"] = "-1";
+			foreach(Match m in ms) {
+				dict[m.Groups[1].Value] = m.Groups[2].Value;
+			}
+			text = Regex.Replace(text, @"(mathParser\..*) = (\d+);", "");
+			var keys = dict.Keys.OrderByDescending(q => q.Length).ToList();
+			foreach(var item in keys) { 
+				text = Regex.Replace(text, @$"\b{item}\b", dict[item]);
+			}
+			text = Regex.Replace(text, @"([A-Z]*\(\) \{)[\r\n\t ]*(return this\.getToken\()", "$1$2");
+			text = Regex.Replace(text, @"([A-Z]*\(\) \{return this\.getToken.*?;)[\r\n\t ]*};", "$1};");
+			text = Regex.Replace(text, @"([A-Z]*\(\) \{return this\.getToken.*?};)", "//$1");
+			text = text.Replace("//PARAMETER()", "PARAMETER()");
+			text = text.Replace("//NUM()", "NUM()");
+			text = text.Replace("//STRING()", "STRING()");
 
-            var lines = text.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
-            ParserJsHelper.RemoveState(lines);
-            //ParserJsHelper.RemoveState2(lines);
-            ParserJsHelper.MiniAccept(lines);
-            ParserJsHelper.MiniExpr(lines);
-            ParserJsHelper.MiniToken(lines);
-            ParserJsHelper.RemovePrecpred(lines);
-            ParserJsHelper.Remove_fun2Context(lines);
-            ParserJsHelper.Remove_sempred(lines);
-            ParserJsHelper.Remove_expr_sempred(lines);
-
-            text = string.Join("\r\n", lines);
-            text = MiniSerializedATN.MiniAtn(text);
-            text = ParserJsHelper.RemoveOthers(text);
-
-            text = text.Replace("export default", "");
-            text = text.Replace("import antlr4 from 'antlr4';", "antlr4=require('./index')");
-            text += "\r\nexports.mathParser = mathParser;";
-            File.WriteAllText("mathParser.js", text);
 
 
-            filePath = Path.GetFullPath(@"..\..\..\..\..\..\g4\bin\mathLexer.js");
-            text = File.ReadAllText(filePath);
-            lines = text.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
-            LexerJsHelper.MiniToken(lines);
+			File.WriteAllText("mathParser.js", text);
 
-            text = string.Join("\r\n", lines);
-            text = LexerJsHelper.RemoveOthers(text);
-            text = MiniSerializedATN.MiniAtn(text);
 
-            text = text.Replace("export default", "");
-            text = text.Replace("import antlr4 from 'antlr4';", "antlr4=require('./index')");
-            text += "\r\nexports.mathLexer = mathLexer;";
 
-            File.WriteAllText("mathLexer.js", text);
 
-        }
-    }
+		}
+	}
 }
