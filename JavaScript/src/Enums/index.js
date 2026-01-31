d6955cb5b51831a7606c4782bd09ead170fe20c7
linzhijun <at6180686@qq.com>
2026-01-31 14:46:34 +0800

    fix

--- JavaScript/src/Enums/index.js
+++ JavaScript/src/Enums/index.js
@@ -3,6 +3,5 @@ export * from './CalculateTreeType.js';
 export * from './ConditionTreeType.js';
 export * from './DistanceUnitType.js';
 export * from './MassUnitType.js';
-export * from './NumberUnitType.js';
 export * from './OperandType.js';
 export * from './VolumeUnitType.js';
