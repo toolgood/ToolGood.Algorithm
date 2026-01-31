06daf2df530f47eb9c7dfbf9144a5e91eb491534
LinZhijun <toolgood@qq.com>
2026-01-31 19:10:52 +0800

    修改错误名称

--- csharp/ToolGood.Algorithm/Internals/Functions/CsharpWeb/Function_TEXTTOBASE64URL.cs
+++ csharp/ToolGood.Algorithm/Internals/Functions/CsharpWeb/Function_TEXTTOBASE64URL.cs
@@ -11,20 +11,20 @@ namespace ToolGood.Algorithm.Internals.Functions.CsharpWeb
 
 		public override Operand Evaluate(AlgorithmEngine work, Func<AlgorithmEngine, string, Operand> tempParameter)
 		{
-			var args1 = func1.Evaluate(work, tempParameter); if(args1.IsNotText) { args1 = args1.ToText("Function '{0}' parameter {1} is error!", "TextToBase64", 1); if(args1.IsError) return args1; }
+			var args1 = func1.Evaluate(work, tempParameter); if(args1.IsNotText) { args1 = args1.ToText("Function '{0}' parameter {1} is error!", "TextToBase64url", 1); if(args1.IsError) return args1; }
 			try {
 				Encoding encoding;
 				if(func2 == null) {
 					encoding = Encoding.UTF8;
 				} else {
-					var args2 = func2.Evaluate(work, tempParameter); if(args2.IsNotText) { args2 = args2.ToText("Function '{0}' parameter {1} is error!", "TextToBase64", 2); if(args2.IsError) return args2; }
+					var args2 = func2.Evaluate(work, tempParameter); if(args2.IsNotText) { args2 = args2.ToText("Function '{0}' parameter {1} is error!", "TextToBase64url", 2); if(args2.IsError) return args2; }
 					encoding = Encoding.GetEncoding(args2.TextValue);
 				}
 				var bytes = encoding.GetBytes(args1.TextValue);
 				var t = Base64.ToBase64ForUrlString(bytes);
 				return Operand.Create(t);
 			} catch(Exception) { }
-			return Operand.Error("Function 'TextToBase64' is error!");
+			return Operand.Error("Function 'TextToBase64url' is error!");
 		}
 		public override void ToString(StringBuilder stringBuilder, bool addBrackets)
 		{
