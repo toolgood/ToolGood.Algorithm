c9b9efe76e82f1f8ed829e8a051945eccf763c55
linzhijun <at6180686@qq.com>
2026-02-05 10:41:20 +0800

    fix

--- csharp/ToolGood.Algorithm/Internals/Functions/Function_N.cs
+++ csharp/ToolGood.Algorithm/Internals/Functions/Function_N.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace ToolGood.Algorithm.Internals.Functions
 {
@@ -27,6 +28,45 @@ namespace ToolGood.Algorithm.Internals.Functions
 			}
 			stringBuilder.Append(')');
 		}
+
+		#region Get
+		protected Operand GetText(AlgorithmEngine work, Func<AlgorithmEngine, string, Operand> tempParameter, int idx)
+		{
+			var args1 = funcs[idx].Evaluate(work, tempParameter);
+			if(args1.IsNotText) return ConvertToText(args1, Name, idx);
+			return args1;
+		}
+
+		protected Operand GetNumber(AlgorithmEngine work, Func<AlgorithmEngine, string, Operand> tempParameter, int idx)
+		{
+			var args1 = funcs[idx].Evaluate(work, tempParameter);
+			if(args1.IsNotNumber) return ConvertToNumber(args1, Name, idx);
+			return args1;
+		}
+
+		protected Operand GetDate(AlgorithmEngine work, Func<AlgorithmEngine, string, Operand> tempParameter, int idx)
+		{
+			var args1 = funcs[idx].Evaluate(work, tempParameter);
+			if(args1.IsNotDate) return ConvertToDate(args1, Name, idx);
+			return args1;
+		}
+
+		protected Operand GetBoolean(AlgorithmEngine work, Func<AlgorithmEngine, string, Operand> tempParameter, int idx)
+		{
+			var args1 = funcs[idx].Evaluate(work, tempParameter);
+			if(args1.IsNotBoolean) return ConvertToBoolean(args1, Name, idx);
+			return args1;
+		}
+		protected Operand GetArray(AlgorithmEngine work, Func<AlgorithmEngine, string, Operand> tempParameter, int idx)
+		{
+			var args1 = funcs[idx].Evaluate(work, tempParameter);
+			if(args1.IsNotArray) return ConvertToArray(args1, Name, idx);
+			return args1;
+		}
+		#endregion
+
+
+
 	}
 
 
