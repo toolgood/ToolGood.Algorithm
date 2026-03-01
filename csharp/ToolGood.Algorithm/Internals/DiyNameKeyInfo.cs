a295e6a2560b34f80609526bfd543ae0b6d76b6b
ToolGood <toolgood@qq.com>
2026-03-01 15:00:03 +0800

    fix

--- csharp/ToolGood.Algorithm/Internals/DiyNameKeyInfo.cs
+++ csharp/ToolGood.Algorithm/Internals/DiyNameKeyInfo.cs
@@ -8,15 +8,15 @@ namespace ToolGood.Algorithm.Internals
 		/// <summary>
 		/// 名称
 		/// </summary>
-		public string Name { get; set; }
+		public string Name { get; internal set; }
 		/// <summary>
 		/// 开始位置
 		/// </summary>
-		public int Start { get; set; }
+		public int Start { get; internal set; }
 		/// <summary>
 		/// 结束位置
 		/// </summary>
-		public int End { get; set; }
+		public int End { get; internal set; }
 		///
 		public override string ToString()
 		{
