aa8c3490150dfb373f831b51fc97a14e208580e9
linzhijun <at6180686@qq.com>
2026-03-03 12:18:24 +0800

    fix

---
+++ csharp/ToolGood.Algorithm/Internals/Functions/MathSum/Function_SUMX2PY2.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace ToolGood.Algorithm.Internals.Functions.MathSum
+{
+	internal sealed class Function_SUMX2PY2 : Function_N
+	{
+		public Function_SUMX2PY2(FunctionBase[] funcs) : base(funcs) { }
+
+		public override string Name => "SUMX2PY2";
+
+		public override Operand Evaluate(AlgorithmEngine engine, Func<AlgorithmEngine, string, Operand> tempParameter)
+		{
+			if (funcs.Length < 2) return ParameterError(1);
+
+			var arrayXArg = GetArray(engine, tempParameter, 0);
+			if (arrayXArg.IsError) return arrayXArg;
+			var arrayX = new List<decimal>();
+			foreach (var item in arrayXArg.ArrayValue) {
+				if (item.IsNumber) {
+					arrayX.Add(item.NumberValue);
+				}
+			}
+
+			var arrayYArg = GetArray(engine, tempParameter, 1);
+			if (arrayYArg.IsError) return arrayYArg;
+			var arrayY = new List<decimal>();
+			foreach (var item in arrayYArg.ArrayValue) {
+				if (item.IsNumber) {
+					arrayY.Add(item.NumberValue);
+				}
+			}
+
+			int minLength = arrayX.Count < arrayY.Count ? arrayX.Count : arrayY.Count;
+
+			decimal result = 0;
+			for (int i = 0; i < minLength; i++) {
+				result += arrayX[i] * arrayX[i] + arrayY[i] * arrayY[i];
+			}
+
+			return Operand.Create(result);
+		}
+	}
+}
