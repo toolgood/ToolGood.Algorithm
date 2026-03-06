40cc8f06d4aab5d1a3c2752707543c17c6abd0df
linzhijun <at6180686@qq.com>
2026-03-06 15:32:33 +0800

    fix

--- csharp/ToolGood.Algorithm/Internals/Functions/MathSum2/Function_BESSELK.cs
+++ csharp/ToolGood.Algorithm/Internals/Functions/MathSum2/Function_BESSELK.cs
@@ -22,8 +22,8 @@ namespace ToolGood.Algorithm.Internals.Functions.MathSum2
 			var args2 = GetNumber_2(engine, tempParameter);
 			if(args2.IsError) { return args2; }
 
-			var x = args1.DoubleValue;
-			var n = (int)Math.Truncate(args2.DoubleValue);
+			var x = args1.NumberValue;
+			var n = (int)Math.Truncate(args2.NumberValue);
 
 			if(x <= 0) {
 				return FunctionError();
@@ -32,19 +32,19 @@ namespace ToolGood.Algorithm.Internals.Functions.MathSum2
 			return Operand.Create(BesselK(n, x));
 		}
 
-		private static double BesselK(int n, double x)
+		private static decimal BesselK(int n, decimal x)
 		{
 			if(n < 0) n = -n;
 
 			if(n == 0) return BesselK0(x);
 			if(n == 1) return BesselK1(x);
 
-			double K0 = BesselK0(x);
-			double K1 = BesselK1(x);
-			double Kn = 0;
+			decimal K0 = BesselK0(x);
+			decimal K1 = BesselK1(x);
+			decimal Kn = 0;
 
 			for(int k = 1; k < n; k++) {
-				Kn = K1 + 2.0 * k / x * K0;
+				Kn = K1 + 2.0m * k / x * K0;
 				K0 = K1;
 				K1 = Kn;
 			}
@@ -52,66 +52,66 @@ namespace ToolGood.Algorithm.Internals.Functions.MathSum2
 			return K1;
 		}
 
-		private static double BesselK0(double x)
+		private static decimal BesselK0(decimal x)
 		{
-			if(x <= 2.0) {
-				double y1 = x * x / 4.0;
-				double ans = -Math.Log(x / 2.0) * BesselI0(x) + (-0.57721566
-					+ y1 * (0.42278420 + y1 * (0.23069756 + y1 * (0.03488590
-					+ y1 * (0.00262698 + y1 * (0.00010750 + y1 * 0.00000740))))));
+			if(x <= 2.0m) {
+				decimal y1 = x * x / 4.0m;
+				decimal ans = -MathEx.Log(x / 2.0m) * BesselI0(x) + (-0.57721566m
+					+ y1 * (0.42278420m + y1 * (0.23069756m + y1 * (0.03488590m
+					+ y1 * (0.00262698m + y1 * (0.00010750m + y1 * 0.00000740m))))));
 				return ans;
 			}
-			double y2 = 2.0 / x;
-			double ans2 = Math.Exp(-x / Math.E) / Math.Sqrt(x) * (1.25331414
-				+ y2 * (-0.07832358 + y2 * (0.02189568 + y2 * (-0.01062446
-				+ y2 * (0.00587872 + y2 * (-0.00251540 + y2 * 0.00053208))))));
+			decimal y2 = 2.0m / x;
+			decimal ans2 = MathEx.Exp(-x / MathEx.E) / MathEx.Sqrt(x) * (1.25331414m
+				+ y2 * (-0.07832358m + y2 * (0.02189568m + y2 * (-0.01062446m
+				+ y2 * (0.00587872m + y2 * (-0.00251540m + y2 * 0.00053208m))))));
 			return ans2;
 		}
 
-		private static double BesselK1(double x)
+		private static decimal BesselK1(decimal x)
 		{
-			if(x <= 2.0) {
-				double y1 = x * x / 4.0;
-				double ans = Math.Log(x / 2.0) * BesselI1(x) + (1.0 / x) * (1.0
-					+ y1 * (0.15443144 + y1 * (-0.67278579 + y1 * (-0.18156897
-					+ y1 * (-0.01919402 + y1 * (0.00110404 + y1 * 0.00004686))))));
+			if(x <= 2.0m) {
+				decimal y1 = x * x / 4.0m;
+				decimal ans = MathEx.Log(x / 2.0m) * BesselI1(x) + (1.0m / x) * (1.0m
+					+ y1 * (0.15443144m + y1 * (-0.67278579m + y1 * (-0.18156897m
+					+ y1 * (-0.01919402m + y1 * (0.00110404m + y1 * 0.00004686m))))));
 				return ans;
 			}
-			double y2 = 2.0 / x;
-			double ans2 = Math.Exp(-x / Math.E) / Math.Sqrt(x) * (1.25331414
-				+ y2 * (0.23498619 + y2 * (-0.03655620 + y2 * (0.01504268
-				+ y2 * (-0.00780353 + y2 * (0.00325614 + y2 * (-0.00068245)))))));
+			decimal y2 = 2.0m / x;
+			decimal ans2 = MathEx.Exp(-x / MathEx.E) / MathEx.Sqrt(x) * (1.25331414m
+				+ y2 * (0.23498619m + y2 * (-0.03655620m + y2 * (0.01504268m
+				+ y2 * (-0.00780353m + y2 * (0.00325614m + y2 * (-0.00068245m)))))));
 			return ans2;
 		}
 
-		private static double BesselI0(double x)
+		private static decimal BesselI0(decimal x)
 		{
-			double ax = Math.Abs(x);
-			if(ax < 3.75) {
-				double y1 = x / 3.75;
+			decimal ax = Math.Abs(x);
+			if(ax < 3.75m) {
+				decimal y1 = x / 3.75m;
 				y1 *= y1;
-				return 1.0 + y1 * (3.5156229 + y1 * (3.0899424 + y1 * (1.2067492
-					+ y1 * (0.2659732 + y1 * (0.0360768 + y1 * 0.0045813)))));
+				return 1.0m + y1 * (3.5156229m + y1 * (3.0899424m + y1 * (1.2067492m
+					+ y1 * (0.2659732m + y1 * (0.0360768m + y1 * 0.0045813m)))));
 			}
-			double y2 = 3.75 / ax;
-			return (Math.Exp(ax / Math.E) / Math.Sqrt(ax)) * (0.39894228 + y2 * (0.01328592
-				+ y2 * (0.00225319 + y2 * (-0.00157565 + y2 * (0.00916281
-				+ y2 * (-0.02057706 + y2 * (0.02635537 + y2 * (-0.01647633 + y2 * 0.00392377))))))));
+			decimal y2 = 3.75m / ax;
+			return (MathEx.Exp(ax / MathEx.E) / MathEx.Sqrt(ax)) * (0.39894228m + y2 * (0.01328592m
+				+ y2 * (0.00225319m + y2 * (-0.00157565m + y2 * (0.00916281m
+				+ y2 * (-0.02057706m + y2 * (0.02635537m + y2 * (-0.01647633m + y2 * 0.00392377m))))))));
 		}
 
-		private static double BesselI1(double x)
+		private static decimal BesselI1(decimal x)
 		{
-			double ax = Math.Abs(x);
-			if(ax < 3.75) {
-				double y1 = x / 3.75;
+			decimal ax = Math.Abs(x);
+			if(ax < 3.75m) {
+				decimal y1 = x / 3.75m;
 				y1 *= y1;
-				return x * (0.5 + y1 * (0.87890594 + y1 * (0.51498869 + y1 * (0.15084934
-					+ y1 * (0.02658733 + y1 * (0.00301532 + y1 * 0.00032411))))));
+				return x * (0.5m + y1 * (0.87890594m + y1 * (0.51498869m + y1 * (0.15084934m
+					+ y1 * (0.02658733m + y1 * (0.00301532m + y1 * 0.00032411m))))));
 			}
-			double y2 = 3.75 / ax;
-			double ans = (Math.Exp(ax / Math.E) / Math.Sqrt(ax)) * (0.39894228 + y2 * (-0.03988024
-				+ y2 * (-0.00362018 + y2 * (0.00163801 + y2 * (-0.01031555
-				+ y2 * (0.02282967 + y2 * (-0.02895312 + y2 * (0.01787654 - y2 * 0.00420059))))))));
+			decimal y2 = 3.75m / ax;
+			decimal ans = (MathEx.Exp(ax / MathEx.E) / MathEx.Sqrt(ax)) * (0.39894228m + y2 * (-0.03988024m
+				+ y2 * (-0.00362018m + y2 * (0.00163801m + y2 * (-0.01031555m
+				+ y2 * (0.02282967m + y2 * (-0.02895312m + y2 * (0.01787654m - y2 * 0.00420059m))))))));
 			return (x < 0) ? -ans : ans;
 		}
 	}
