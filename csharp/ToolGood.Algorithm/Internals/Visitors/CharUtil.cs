5a64ee843666a0dc47bed483aa464dd5ef9ad7ec
linzhijun <at6180686@qq.com>
2026-03-12 13:11:57 +0800

    fix

--- csharp/ToolGood.Algorithm/Internals/Visitors/CharUtil.cs
+++ csharp/ToolGood.Algorithm/Internals/Visitors/CharUtil.cs
@@ -4,31 +4,48 @@ namespace ToolGood.Algorithm.Internals.Visitors
 {
 	internal static class CharUtil
 	{
+#if NETSTANDARD2_1
 		public static char StandardChar(char o)
 		{
-			if(o < 'a') return o;  // '[', ']', '^', '_', '`' 等字符保持不变
-			if(o <= 'z') return (char)(o - 32); // a-z 直接转大写，避免方法调用
-			if(o < 127) return o; // 其他 ASCII 字符保持不变
-			if(o <= 65280) {
-				switch(o) {
-					case (char)215: return '*';  // ×
-					case (char)247: return '/';  // ÷
-					case (char)8216: return '\''; // '
-					case (char)8217: return '\''; // '
-					case (char)8220: return '"';  // "
-					case (char)8221: return '"';  // "
-					case (char)12288: return (char)32;
-					case (char)12304: return '['; // 【
-					case (char)12305: return ']'; // 】
-					case (char)12308: return '('; // 〔
-					case (char)12309: return ')'; // 〕
-					default: return o;
-				}
-			} else if(o < 65375) {
+			if(o < 'a') return o;
+			if(o <= 'z') return (char)(o - 32);
+			if(o < 127) return o;
+			switch(o) {
+				case (char)215: return '*';
+				case (char)247: return '/';
+				case (char)8216:
+				case (char)8217: return '\'';
+				case (char)8220:
+				case (char)8221: return '"';
+				case (char)12288: return ' ';
+				case (char)12304: return '[';
+				case (char)12305: return ']';
+				case (char)12308: return '(';
+				case (char)12309: return ')';
+			}
+			if(o > 65280 && o < 65375) {
 				o = (char)(o - 65248);
 			}
 			return char.ToUpperInvariant(o);
 		}
+#else
+		public static char StandardChar(char o) => o switch
+		{
+			>= 'a' and <= 'z' => (char)(o - 32),
+			< (char)127 => o,
+			(char)215 => '*',
+			(char)247 => '/',
+			(char)8216 or (char)8217 => '\'',
+			(char)8220 or (char)8221 => '"',
+			(char)12288 => ' ',
+			(char)12304 => '[',
+			(char)12305 => ']',
+			(char)12308 => '(',
+			(char)12309 => ')',
+			> (char)65280 and < (char)65375 => (char)(o - 65248),
+			_ => char.ToUpperInvariant(o)
+		};
+#endif
  
 		public static bool Equals(string left, char right)
 		{
