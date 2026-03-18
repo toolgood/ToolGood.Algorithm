709936c4d13e40fc37d3815a2320de591dd4dafb
linzhijun <at6180686@qq.com>
2026-03-18 12:48:58 +0800

    修复 Java 版本构造函数，与 C# 版本保持一致

--- java/src/main/java/toolgood/algorithm/internals/functions/csharpsecurity/Function_SHA256.java
+++ java/src/main/java/toolgood/algorithm/internals/functions/csharpsecurity/Function_SHA256.java
@@ -15,8 +15,8 @@ import toolgood.algorithm.internals.functions.FunctionBase;
 import toolgood.algorithm.internals.functions.NoneEngine;
 
 public final class Function_SHA256 extends Function_1 {
-    public Function_SHA256(FunctionBase[] funcs) {
-        super(funcs);
+    public Function_SHA256(FunctionBase func1) {
+        super(func1);
     }
 
     @Override
