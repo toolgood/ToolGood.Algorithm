2b8ef7198a9ea518072309720f14bd23fd611cca
linzhijun <at6180686@qq.com>
2026-03-19 14:41:30 +0800

    为了兼容其他语言版本，使用 IAntlrErrorListener<T>接口

---
+++ csharp/ToolGood.Algorithm/Internals/Visitors/AntlrErrorListener.cs
@@ -0,0 +1,27 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ToolGood.Algorithm.Internals.Visitors
+{
+	internal sealed class AntlrErrorListener<Symbol> : IAntlrErrorListener<Symbol>
+	{
+		public AntlrErrorData Data { get; set; }
+		public AntlrErrorListener(AntlrErrorData data)
+		{
+			Data = data;
+		}
+
+		public void SyntaxError(TextWriter output, IRecognizer recognizer, Symbol offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+		{
+			Data.IsError = true;
+			Data.ErrorMsg = msg;
+		}
+	}
+	internal sealed class AntlrErrorData
+	{
+		public bool IsError { get; set; }
+		public string ErrorMsg { get; set; }
+	}
+}
