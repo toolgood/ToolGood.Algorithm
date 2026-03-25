ada24105a1adda8aca04f5afe7a6c914b9a30fe4
linzhijun <at6180686@qq.com>
2026-03-25 08:42:38 +0800

    fix

--- csharp/ToolGood.Algorithm/Internals/Functions/MathBase/Function_Percentage.cs
+++ csharp/ToolGood.Algorithm/Internals/Functions/MathBase/Function_Percentage.cs
@@ -17,7 +17,7 @@ namespace ToolGood.Algorithm.Internals.Functions.MathBase
         public override Operand Evaluate(AlgorithmEngine engine, Func<AlgorithmEngine, string, Operand> tempParameter)
         {
             var args1 = GetNumber_1(engine, tempParameter);
-            if (args1.IsErrorOrNone || args1.IsNone) { return args1; }
+            if (args1.IsErrorOrNone) { return args1; }
             return Operand.Create(args1.NumberValue / 100.0m);
         }
         public override void ToString(StringBuilder stringBuilder, bool addBrackets)
