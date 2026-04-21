668ab546e3ceb590f4c89111e8fb40c06bd8caab
linzhijun <at6180686@qq.com>
2026-04-21 13:47:30 +0800

    fix

---
+++ csharp/Antlr4Helper.CSharpHelper/Trees/DfaTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Antlr4Helper.CSharpHelper.Trees
+{
+	public class DfaTree
+	{
+		public byte[] _dict;
+		public byte[] _key;
+		public ushort[] _next;
+		public byte[] _end; //0,无匹配，0xff 跳过
+
+		public DfaTree() { }
+
+
+		//public List<string> FindAll(string text)
+		//{
+		//	List<string> root = new List<string>();
+		//	var p = 0;
+
+		//	foreach(char t1 in text) {
+		//		var t = (char)_dict[t1];
+		//		if(t == 0) {
+		//			p = 0;
+		//			continue;
+		//		}
+		//		var next = _next[p] + t;
+		//		bool find = _key[next] == t;
+		//		if(find == false && p != 0) {
+		//			p = 0;
+		//			next = _next[0] + t;
+		//			find = _key[next] == t;
+		//		}
+		//		if(find) {
+		//			var index = _check[next];
+		//			if(index > 0) {
+		//				foreach(var item in _guides[index]) {
+		//					root.Add(_keywords[item]);
+		//				}
+		//			}
+		//			p = next;
+		//		}
+		//	}
+		//	return root;
+		//}
+
+
+
+		private void build(List<TrieNodeEx> nodes, Int32 length)
+		{
+			int[] has = new int[0x00FFFFFF];
+			bool[] seats = new bool[0x00FFFFFF];
+			bool[] seats2 = new bool[0x00FFFFFF];
+			Int32 start = 1;
+			Int32 oneStart = 1;
+			for(int i = 0; i < nodes.Count; i++) {
+				var node = nodes[i];
+				node.Rank(ref oneStart, ref start, seats, seats2, has);
+			}
+			Int32 maxCount = has.Length - 1;
+			while(has[maxCount] == 0) { maxCount--; }
+			length = maxCount + length + 1;
+
+			_key = new byte[length];
+			_next = new ushort[length];
+			_end = new byte[length];
+			for(Int32 i = 0; i < length; i++) {
+				var item = nodes[has[i]];
+				if(item == null) continue;
+				_key[i] =(byte) item.Char;
+				_next[i] =(ushort) item.Next;
+				if(item.End) {
+					_end[i] = (byte)item.Results[0];
+				}
+			}
+		}
+
+
+	}
+}
---
+++ csharp/Antlr4Helper.CSharpHelper/Trees/TrieNodeEx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Antlr4Helper.CSharpHelper.Trees
+{
+	sealed class TrieNodeEx
+	{
+		internal int Char;
+		public bool End { get { return Results != null; } }
+		internal int Index;
+		internal List<Int32> Results;
+		internal Dictionary<int, TrieNodeEx> m_values;
+		private Int32 minflag = Int32.MaxValue;
+		private Int32 maxflag = 0;
+		internal Int32 Next;
+		public int Count;
+
+
+		public void Add(int c, TrieNodeEx node3)
+		{
+			if(minflag > c) { minflag = c; }
+			if(maxflag < c) { maxflag = c; }
+			if(m_values == null) {
+				m_values = new Dictionary<int, TrieNodeEx>();
+			}
+			m_values.Add(c, node3);
+			Count++;
+		}
+
+		public void SetResults(Int32 text)
+		{
+			if(Results == null) {
+				Results = new List<int>();
+				Results.Add(text);
+			}
+		}
+
+		public bool HasKey(int c)
+		{
+			if(m_values == null) {
+				return false;
+			}
+			return m_values.ContainsKey(c);
+		}
+
+		public int Rank(ref Int32 oneStart, ref Int32 start, bool[] seats, bool[] seats2, int[] has)
+		{
+			if(maxflag == 0) return 0;
+			if(minflag == maxflag) {
+				RankOne(ref oneStart, seats, has);
+				return 0;
+			}
+			var keys = m_values.Select(q => (Int32)q.Key).OrderByDescending(q => q).ToList();
+			var length = keys.Count - 1;
+			int[] moves = new int[keys.Count - 1];
+			for(int i = 1; i < keys.Count; i++) {
+				moves[i - 1] = maxflag - keys[i];
+			}
+
+			while(has[start] != 0) { start++; }
+			var s = start < (Int32)minflag ? (Int32)minflag : start;
+			var next = s - minflag;
+			var e = next + maxflag;
+			while(e < has.Length) {
+				if(seats2[e] == false && seats[next] == false) {
+					var isok = true;
+					for(int i = 0; i < keys.Count; i++) {
+						var position = next + keys[i];
+						if(has[position] > 0) {
+							for(int j = 0; j < length; j++) {
+								seats2[position + moves[j]] = true;
+							}
+							isok = false;
+							break;
+						}
+					}
+					if(isok) {
+						SetSeats(next, seats, has);
+						start += keys.Count / 2;
+						Array.Clear(seats2, start, e + maxflag - start + 1);
+						return next;
+					}
+				}
+				next++;
+				e++;
+			}
+			throw new Exception("");
+		}
+
+		private void RankOne(ref int start, bool[] seats, int[] has)
+		{
+			while(has[start] != 0) { start++; }
+			var s = start < (Int32)minflag ? (Int32)minflag : start;
+
+			for(Int32 i = s; i < has.Length; i++) {
+				if(has[i] == 0) {
+					var next = i - (Int32)minflag;
+					if(seats[next]) continue;
+					SetSeats(next, seats, has);
+					break;
+				}
+			}
+			start++;
+		}
+
+
+		private void SetSeats(Int32 next, bool[] seats, int[] has)
+		{
+			Next = next;
+			seats[next] = true;
+
+			if(m_values != null) {
+				foreach(var item in m_values) {
+					var position = next + item.Key;
+					has[position] = item.Value.Index;
+				}
+			}
+
+		}
+
+
+	}
+}
